70b9518202d9b783a97804abbe9dc229fa91c158
ontdexio <48110635+ontdexio@users.noreply.github.com>
2019-03-03 22:51:28 +0800

    Create config.go

---
+++ config/config.go
@@ -0,0 +1,37 @@
+package config
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+)
+
+type Config struct {
+	Wallet     string
+	Password   string
+	Contract   string
+	To         string
+	Amount     uint64
+	Rpc        string
+	TxNum      uint // whole tx num is *TxFactor
+	TxFactor   uint
+	RoutineNum uint // whole tx save to RoutineNum files, and one go-routine per file
+	TPS        uint
+	GasPrice   uint64
+	GasLimit   uint64
+	SaveTx     bool
+	SendTx     bool
+}
+
+func ParseConfig(path string) (*Config, error) {
+	fileContent, err := ioutil.ReadFile(path)
+	if err != nil {
+		return nil, fmt.Errorf("ParseConfig: failed, err: %s", err)
+	}
+	config := &Config{}
+	err = json.Unmarshal(fileContent, config)
+	if err != nil {
+		return nil, fmt.Errorf("ParseConfig: failed, err: %s", err)
+	}
+	return config, nil
+}
